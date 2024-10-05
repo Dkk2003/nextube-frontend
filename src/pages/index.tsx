8ee5e9915e09c47c22ff1c6b862a7486820acc9a
Dkk2003 <darshan1010prajapati@gmail.com>
2024-10-05 13:08:36 +0530

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,24 @@
 import UserAPI from "@/axios/user";
 import Logo from "../components/Logo";
 import { useEffect } from "react";
-import { authanticatedRoute, unauthanticatedRoute } from "@/utils/authguard";
-
+import Cookies from "js-cookie";
+import { useUser } from "@/contexts/auth";
 const Home = () => {
+  const { user } = useUser();
   useEffect(() => {
     UserAPI.loginUser({
       email: "darshan1010prajapati@gmail.com",
       password: "Test@123",
     }).then((res) => {
       console.log(res.data);
+      Cookies.set("accessToken", res?.data?.accessToken as string);
     });
   }, []);
 
   return (
     <div className="w-full h-screen flex flex-col gap-3 justify-center items-center">
       <Logo />
+      {user?.fullName}
       <p className="text-lg">Coming Soon</p>
     </div>
   );
